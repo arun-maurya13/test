0e151a1fdef78b84d986648416161645f1d86d26
arun-maurya13 <131513742+arun-maurya13@users.noreply.github.com>
2023-04-25 16:33:42 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/example/demo/HelloWorldApplication.java
+++ src/main/java/com/example/demo/HelloWorldApplication.java
@@ -2,13 +2,28 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
-public class HelloWorldApplication {
+public class HelloWorld1Application extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldApplication.class, args);
-		System.out.println("HELLO WORLD");
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HelloWorld1Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorld1Application.class);
+    }
+
+
+    @RequestMapping("/")
+    String helloWorld() {
+        return "hello Arun";
+    }
 
 }
